a9065234836f1dbe2e96c6665a7ba45501fdbe01
Myrnova <debora_cavallari@hotmail.com>
2021-01-12 08:16:43 -0300

    first commit

---
+++ src/pages/SignUp/index.tsx
@@ -0,0 +1,16 @@
+import React, { Component } from 'react'
+import { Text, View } from 'react-native'
+
+import { Container } from './styles'
+
+const SignUp: React.FC = () => {
+
+    return (
+      <Container>
+        <Text> textInComponent </Text>
+      </Container>
+    )
+
+}
+
+export default SignUp
